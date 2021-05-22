3d20811a57db30fb58112e2081b5628fc6013e14
Cyakat <noah@thebauers.net>
2021-05-22 13:35:08 -0600

    working on a new cache system

--- commands/global.js
+++ commands/global.js
@@ -3,75 +3,86 @@ module.exports = {
   description: 'gets the global leaderboard',
   execute(message, args, Discord, client, con) {
 
-    //displays that the leaderboard is loading because sometimes the query can take a bit
-    let embed = new Discord.MessageEmbed()
-    .setColor(0xBF66E3)
-    .setTitle('Global Leaderboard')
-    .setDescription('Loading leaderboard')
-    .setFooter('Requested by ' + message.author.tag);
-    async function loading(message, embed) {
-      let msg = await message.channel.send(embed);
-      setTimeout(() => {
-        msg.edit(embed);
-      }, 1);
-    }
-    loading(message, embed);
+    let nextUpdateTime;
+    let currentDate = Date.now()
 
-      //gets the entire users database and sorts through it
-      con.query("SELECT id, SUM(words) AS 'words' FROM users GROUP BY id ORDER BY words DESC;", (err, response) => {
+    if(nextUpdateTime < currentDate) {
 
 
-          //this is for changing the pages in the global leaderboard. this broke i don't know why
-          let inTop = false;
-          let pos = 1;
-          let o = 0;
-          let set = args[1];
-          if(set === undefined) {
-            set = 0;
-          }
-          set = (set * 10) - 10;
-          if (set < 0) {
-            set = 0;
-          }
-          setpos = set/10;
+      //displays that the leaderboard is loading because sometimes the query can take a bit
+      let embed = new Discord.MessageEmbed()
+      .setColor(0xBF66E3)
+      .setTitle('Global Leaderboard')
+      .setDescription('Loading leaderboard')
+      .setFooter('Requested by ' + message.author.tag);
+      async function loading(message, embed) {
+        let msg = await message.channel.send(embed);
+        setTimeout(() => {
+          msg.edit(embed);
+        }, 1);
+      }
+      loading(message, embed);
 
-          //formats the embed with the data given by the query
-          for(let i = 0; i < response.length; i++) {
-            try{
-              let user = client.users.cache.get(response[i].id.toString());
-              //get user and server
-              i = parseInt(i);
-              filler = user.username
-              o++;
-              //add user positions, max of 10, from json object
-              if(user.id === message.author.id) {
-                embed.addField('#' + (o) + ' `' + message.author.username + '`', response[i].words);
-                inTop = true;
-              } else {
-                if(o < 11+set & o > 0+set) {
-                  embed.addField('#' + (o) + ' ' + user.username, response[i].words);
+
+
+        //gets the entire users database and sorts through it
+        con.query("SELECT id, SUM(words) AS 'words' FROM users GROUP BY id ORDER BY words DESC;", (err, response) => {
+
+
+            //this is for changing the pages in the global leaderboard. this broke i don't know why
+            let inTop = false;
+            let pos = 1;
+            let o = 0;
+            let set = args[1];
+            if(set === undefined) {
+              set = 0;
+            }
+            set = (set * 10) - 10;
+            if (set < 0) {
+              set = 0;
+            }
+            setpos = set/10;
+
+            //formats the embed with the data given by the query
+            for(let i = 0; i < response.length; i++) {
+              try{
+                let user = client.users.cache.get(response[i].id.toString());
+                //get user and server
+                i = parseInt(i);
+                filler = user.username
+                o++;
+                //add user positions, max of 10, from json object
+                if(user.id === message.author.id) {
+                  embed.addField('#' + (o) + ' `' + message.author.username + '`', response[i].words);
+                  inTop = true;
+                } else {
+                  if(o < 11+set & o > 0+set) {
+                    embed.addField('#' + (o) + ' ' + user.username, response[i].words);
+                  }
                 }
+
+                if(inTop === false && user.id === message.author.id) {
+                  embed.addField('#' + (i+1) + ' `' + message.author.username + '`', response[i].words, true);
+                  break;
+                } else if(pos === 10+setpos) {
+                  break;
+                }
+                pos++;
+              }
+              catch(err) {
+
               }
 
-              if(inTop === false && user.id === message.author.id) {
-                embed.addField('#' + (i+1) + ' `' + message.author.username + '`', response[i].words, true);
-                break;
-              } else if(pos === 10+setpos) {
-                break;
-              }
-              pos++;
             }
-            catch(err) {
+            embed.setDescription('The top-sending users world-wide\nThis uses a collection of all messages these users have sent')
+            //msg.edit(embed);
 
-            }
-
-          }
-          embed.setDescription('The top-sending users world-wide\nThis uses a collection of all messages these users have sent')
-          //msg.edit(embed);
-
-      });
-      //getGlobalTop(message);
-      return;
-
+        });
+        //getGlobalTop(message);
+        return;
+      }
+      else {
+        
+      }
   }
 };
